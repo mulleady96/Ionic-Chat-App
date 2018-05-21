7dacde04d598f61f9ae5e6e4083d7aca44c7107d
Andrew Mulleady <andrewmulleady@live.ie>
2018-05-21 22:33:33 +0100

    full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
+import { SigninPage } from '../pages/signin/signin';
 
-import { HomePage } from '../pages/home/home';
+const config = {
+  apiKey: 'AAAACPIpZ3E:APA91bG22iZBxDw4_7zFXDOM3AtZUh1saiCKk2VbskHU2eefyUFobAogwxvnNJk2PEjL_WHKuw18plhrImCxwpijFPmUzEhYp2d17Dq049qrw86uHcRCrV4f958GKEtn2UMTcSF74FI1',
+  authDomain: 'chatapp-f2f6c.firebaseapp.com',
+  databaseURL: 'https://chatapp-f2f6c.firebaseio.com',
+  projectId: 'chatapp-f2f6c',
+  storageBucket: 'gs://chatapp-f2f6c.appspot.com',
+};
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SigninPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +26,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
+    // Inits firebase app with config settings outlined above -> now ready to be populated with data.
   }
 }
-
